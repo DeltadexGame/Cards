363af9d235615c06c5ee7b6969ea78571b2c1fb8
OisinA <117397783@umail.ucc.ie>
2020-02-17 14:50:10 +0000

    Debug print

--- scripts/2/heavy.go
+++ scripts/2/heavy.go
@@ -8,10 +8,12 @@ import (
 
 func HandleMonsterDamageEvent(event events.Event) (events.Event, bool) {
 	fmt.Println("running")
+	fmt.Println(event.EventInfo)
 	monster := event.EventInfo["monster"].(gameplay.Monster)
 	if monster.Ability.AbilityID != 2 {
 		return event, false
 	}
+	fmt.Println("got to halfing")
 	event.EventInfo["damage"] = event.EventInfo["damage"].(int) / 2
 	fmt.Println("Using heavy ability, damage was halved from " + string(event.EventInfo["damage"].(int)*2) + " to " + string(event.EventInfo["damage"].(int)) + ".")
 	return event, true
